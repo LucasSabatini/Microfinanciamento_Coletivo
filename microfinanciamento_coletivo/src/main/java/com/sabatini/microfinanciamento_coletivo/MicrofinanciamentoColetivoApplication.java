c44bd1d22eabe030af8c65e5454cb8b2dbd813c6
lucassabatini <lucas.frsabatini@gmail.com>
2023-08-11 16:46:03 -0300

    Initial commit

---
+++ microfinanciamento_coletivo/src/main/java/com/sabatini/microfinanciamento_coletivo/MicrofinanciamentoColetivoApplication.java
@@ -0,0 +1,13 @@
+package com.sabatini.microfinanciamento_coletivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrofinanciamentoColetivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrofinanciamentoColetivoApplication.class, args);
+	}
+
+}
---
+++ microfinanciamento_coletivo/src/test/java/com/sabatini/microfinanciamento_coletivo/MicrofinanciamentoColetivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sabatini.microfinanciamento_coletivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrofinanciamentoColetivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
