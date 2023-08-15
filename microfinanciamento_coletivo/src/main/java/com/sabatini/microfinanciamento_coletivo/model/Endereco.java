9be0a3466f7d70ce84e7fad35c1165b747bdc292
marsabatini <marco_rsabatini@hotmail.com>
2023-08-15 16:28:06 -0300

    Criação da classe Endereco.

---
+++ microfinanciamento_coletivo/src/main/java/com/sabatini/microfinanciamento_coletivo/model/Endereco.java
@@ -0,0 +1,132 @@
+package com.sabatini.microfinanciamento_coletivo.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "endereco")
+public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String logradouro;
+
+    private String numero;
+
+    private String complemento;
+
+    private String bairro;
+
+    private String cep;
+
+
+    @OneToMany
+    @JoinColumn(name = "id_user")
+    private List<User> user;
+
+    @OneToOne
+    @JoinColumn(name = "id_organizacao")
+    private Organizacao Organizacao;
+
+
+    // Constructors
+    public Endereco(){
+
+    }
+
+    public Endereco(Long id, String logradouro, String numero, String complemento, String bairro, String cep, List<User> user, Organizacao organizacao) {
+        this.id = id;
+        this.logradouro = logradouro;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.cep = cep;
+        this.user = user;
+        Organizacao = organizacao;
+    }
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public List<User> getUser() {
+        return user;
+    }
+
+    public void setUser(List<User> user) {
+        this.user = user;
+    }
+
+    public Organizacao getOrganizacao() {
+        return Organizacao;
+    }
+
+    public void setOrganizacao(Organizacao organizacao) {
+        Organizacao = organizacao;
+    }
+
+    // Equals and Hash
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Endereco endereco = (Endereco) o;
+        return Objects.equals(id, endereco.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
